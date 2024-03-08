af0b04d3a628a857a47bd65971124068c51c7896
Krystof Komanec <krystof.komanec@gmail.com>
2024-03-08 14:05:56 +0100

    Update version to 4.4

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ project_name_full = 'FAIR Wizard'
 registry_name = 'FAIR Wizard Registry'
 
 # The full version, including alpha/beta/rc tags
-version = release = '4.3'
+version = release = '4.4'
 
 rst_prolog = f"""
 
