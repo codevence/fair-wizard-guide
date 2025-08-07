ca2da6f3fe94f9fd56bf95f10585eec67379b5dd
krystofkomanec <krystof.komanec@gmail.com>
2025-08-07 14:43:44 +0200

    4.21.0

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ project_name_full = 'FAIR Wizard'
 registry_name = 'FAIR Wizard Registry'
 
 # The full version, including alpha/beta/rc tags
-version = release = '4.20'
+version = release = '4.21'
 
 rst_prolog = f"""
 
