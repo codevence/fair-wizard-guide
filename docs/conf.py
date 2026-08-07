ed1678ef2b27953027197ba289f81d36dcacfe9d
krystofkomanec <krystof.komanec@gmail.com>
2026-08-07 10:00:46 +0200

    4.33.0

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2020 - 2026, FAIR Wizard Team'
 author = 'FAIR Wizard Team'
 
 # The full version, including alpha/beta/rc tags
-version = release = '4.32'
+version = release = '4.33'
 
 rst_prolog = f"""
 
